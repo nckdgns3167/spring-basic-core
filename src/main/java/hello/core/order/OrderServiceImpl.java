ced11bd7eb4de8bbf8e12d07b367e2ddc98ae2ba
nckdgns3167 <nckdgns3167@naver.com>
2023-01-28 17:30:15 +0900

    주문과 할인 도메인 실행과 테스트(JUnit)

---
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -0,0 +1,25 @@
+package hello.core.order;
+
+import hello.core.discount.DiscountPolicy;
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.Member;
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+
+public class OrderServiceImpl implements OrderService {
+
+    // 문제점: 추상화, 구체화 모두 의존하고 있는 모습. (DIP 위반)
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+
+    @Override
+
+    public Order createOrder(Long memberId, String itemName, int itemPrice) {
+
+        // SRP이 잘 지켜진 설계
+        // 할인과 관련된 것은 discountPolicy가 알아서 할 일이고 결과만 받도록 되어 있음.
+        Member member =  memberRepository.findById(memberId);
+        int discountPrice = discountPolicy.discount(member, itemPrice);
+        return new Order(memberId, itemName, itemPrice, discountPrice);
+    }
+}
