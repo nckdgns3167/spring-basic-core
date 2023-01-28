9d6df1386fb6be457fc900bd49532c83ebcf1d4c
nckdgns3167 <nckdgns3167@naver.com>
2023-01-28 17:29:52 +0900

    문제저: DIP 위반

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -3,6 +3,7 @@ package hello.core.member;
 // 어떤 인터페이스에 대한 구현체가 딱 하나일 경우에는 그 구현체 이름 뒤에 Impl을 붙여준다.
 public class MemberServiceImpl implements MemberService {
 
+    // 문제점: 추상화, 구체화 모두 의존하고 있는 모습. (DIP 위반)
     private final MemberRepository memberRepository = new MemoryMemberRepository();
 
     @Override
