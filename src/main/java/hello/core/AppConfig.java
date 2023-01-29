a9130e9fa73f59ed5e567ef29d208e54c9dfc481
nckdgns3167 <nckdgns3167@naver.com>
2023-01-29 16:03:31 +0900

    스프링 컨테이너로 전환, 빈 등록 및 사용

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -8,11 +8,14 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 
 // 공연 기획자 역할, "구성 영역"
 // 이 클래스의 역할은 애플리케이션의 실제 동작에 필요한 "구현 객체를 생성"하는 것.
 // 생성한 객체 인스턴스의 레퍼런스를 "생성자를 통해서 주입(연결)"한다.
+@Configuration
 public class AppConfig {
 
     // 역할과 구현 클래스가 한눈에 들어오도록 변경.
@@ -20,11 +23,13 @@ public class AppConfig {
 
     // new MemoryMemberRepository() 코드 중복이 제거됐다.
     // 만약 다른 구현체로 스왑해야할 때 이 부분만 변경하면 된다.
-    private static MemberRepository memberRepository() {
+    @Bean
+    public static MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
-    private static DiscountPolicy discountPolicy() {
+    @Bean
+    public static DiscountPolicy discountPolicy() {
 // 할인 정책을 바꾸었지만 클라이언트 코드인 OrderServiceImpl 를
 // 포함해서 "사용 영역"의 어떤 코드도 변경하지 않았다. "구성 영역"인 이곳만 변경했다.
 // OCP를 지키게 됐다.
@@ -32,10 +37,12 @@ public class AppConfig {
         return new RateDiscountPolicy();
     }
 
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
--- src/test/java/hello/core/member/MemberServiceTest.java
+++ src/test/java/hello/core/member/MemberServiceTest.java
@@ -4,6 +4,8 @@ import hello.core.AppConfig;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberServiceTest {
 
@@ -11,8 +13,10 @@ public class MemberServiceTest {
 
     @BeforeEach
     public void beforeEach() {
-        AppConfig appConfig = new AppConfig();
-        memberService = appConfig.memberService();
+//        AppConfig appConfig = new AppConfig();
+//        memberService = appConfig.memberService();
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        memberService = applicationContext.getBean("memberService", MemberService.class);
     }
 
     @Test
